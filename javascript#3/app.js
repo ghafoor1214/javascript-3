edf5555108409aef6ec536e46b4dbf954382d3fe
Abdul Ghafoor <153913684+ghafoor1214@users.noreply.github.com>
2024-03-23 05:05:51 +0500

    Add files via upload

---
+++ javascript#3/app.js
@@ -0,0 +1,87 @@
+// chapter# 17_18
+
+// Qno#1
+
+// var multidimensionalArray = [];
+
+// var rows = 3;
+// var columns = 4;
+
+// for (var i = 0; i < rows; i++) {
+//     multidimensionalArray[i] = []; 
+//     for (let j = 0; j < columns; j++) {
+//         multidimensionalArray[i][j] = 0; 
+//     }
+// }
+
+// console.log(multidimensionalArray);
+
+// Qno#2
+
+// var matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+
+// console.log(matrix);
+
+// Qno#3
+
+// for (var a = 1; a <= 10; a++) {
+//     console.log(a);
+// }
+
+// Qno#4
+
+// let tableNumber = parseInt(prompt("Enter the number for multiplication table:"));
+
+// let tableLength = parseInt(prompt("Enter the length of the multiplication table:"));
+
+// console.log(`Multiplication Table of ${tableNumber} upto ${tableLength} terms:`);
+// for (let i = 1; i <= tableLength; i++) {
+//     let result = tableNumber * i;
+//     console.log(`${tableNumber} * ${i} = ${result}`);
+// }
+
+// Qno#5
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// console.log("Items of the array using for loop:");
+// for (var a = 0; a < fruits.length; i++) {
+//     console.log(fruits[a]);
+// }
+
+// Qno#6
+
+// document.write("Counting:");
+// for (var a = 1; a <= 15; a++) {
+//     document.write(a + ", ");
+// }
+// document.write("<br>");
+
+// document.write("Reverse counting:");
+// for (var a = 10; a >= 1; a--) {
+//     document.write(a + ", ");
+// }
+// document.write("<br>");
+
+
+// document.write("Even: ");
+// for (var a = 0; a <= 20; a += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// document.write("Odd: ");
+// for (var a = 1; a <= 19; a += 2) {
+//     document.write(a + ", ");
+// }
+// document.write("<br>");
+
+// document.write("Series: ");
+// for (var a = 2; a <= 20; a += 2) {
+//     document.write(a + "k, ");
+// }
+// document.write("<br>");
+
